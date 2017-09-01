3c7ba3d028368bb74913ac78f45437dd38cdb831
Constaniphobia <mvu@aic.ac.nz>
2017-09-01 13:02:52 +1200

    Create MorseTwoButton.cpp

---
+++ MorseTwoButton.cpp
@@ -0,0 +1,17 @@
+#include "Arduino.h"
+#include "MorseTwoButton.h"
+
+MorseTwoButton::MorseTwoButton(int pin1, int pin2)
+{
+  pinMode(pin1, INPUT);
+  pinMode(pin2, INPUT);
+  _pin1 = pin1
+  _pin2 = pin2
+}
+
+void MorseTwoButton::outPUT()
+{
+
+
+
+}
