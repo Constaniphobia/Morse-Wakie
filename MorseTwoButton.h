f2901bea4714c86d1937a193bed41a5f378bd522
Constaniphobia <mvu@aic.ac.nz>
2017-09-01 12:56:55 +1200

    Update MorseTwoButton.h

--- MorseTwoButton.h
+++ MorseTwoButton.h
@@ -1,2 +1,21 @@
 //Header file for Arduino Morse Code
+/*
+  Morse.h - Library for flashing Morse code.
+  Created by David A. Mellis, November 2, 2007.
+  Released into the public domain.
+*/
+#ifndef MorseTwoButton_h
+#define MorseTwoButton_h
 
+#include "Arduino.h"
+
+class MorseTwoButton
+{
+  public:
+    Morse(int pin, int pin);
+    void outCode();
+  private:
+    int _pin;
+};
+
+#endif
